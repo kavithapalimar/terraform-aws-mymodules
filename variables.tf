b0133f8b51bcc5aabed37accef0a00105b222893
kavithapalimar <kavitha.palimar@gmail.com>
2023-04-25 21:09:08 -0400

    Terraform Modules

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-076b159fbf46c67a1"
+}
+variable "amiID" {
+	default="ami-016eb5d644c333ccb"
+}
+variable "keyPair" {
+	default="DevOps_Training"
+}
+variable "keyPair1" {
+	default="DevOps_Training.pem"
+}
+
+
+
+
